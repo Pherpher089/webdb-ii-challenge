caa562968c7a0ae96d1a01fc857cf39060d1c694
Pherpher089 <Christutor089@gmail.com>
2019-08-10 21:56:10 -0700

    Added in the post route

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -12,5 +12,32 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    const {make, model, year, milage, vin} = req.body;
+
+    if(!make) {
+        res.status(400).json({errorMessage: 'please provide the make of the vehicle'});
+    }
+    if(!model) {
+        res.status(400).json({errorMessage: 'please provide the model of the vehicle'});
+    }
+    if(!year) {
+        res.status(400).json({errorMessage: 'please provide the year of the vehicle'});
+    }
+    if(!milage) {
+        res.status(400).json({errorMessage: 'please provide the milage of the vehicle'});
+    }
+    if(!vin) {
+        res.status(400).json({errorMessage: 'please provide the vin of the vehicle'});
+    }
+    
+    try {
+        const newCar = await db('cars').insert(req.body);
+        res.status(200).json(newCar);
+    } catch ({message}) {
+        res.status(500).json(message);
+    }
+})
+
 
 module.exports = router;
